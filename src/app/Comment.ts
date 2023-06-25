107849fc51b1b4241f6716a83fdbf0e970278748
levizoca <64503782+levizoca@users.noreply.github.com>
2023-06-25 14:14:22 -0300

    finish some components and start messaging service

---
+++ src/app/Comment.ts
@@ -0,0 +1,8 @@
+export interface Comment {
+    id?: Number,
+    text: String,
+    username: String,
+    moments_id: Number,
+    created_at?: String,
+    updated_at?: String
+}
--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrls: ['./new-moment.component.css']
 })
-export class NewMomentComponent {
+export class NewMomentComponent implements OnInit {
+  btnText = "Compartilhar";
 
+  constructor(private momentService: MomentService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData()
+
+    formData.append('title', moment.title)
+    formData.append('description', moment.description)
+
+    if(moment.image) {
+      formData.append('image', moment.image)
+    }
+   
+    await this.momentService.createMoment(formData).subscribe()
+
+  }
 }
